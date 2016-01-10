e0dc93400e8ee09237b07cb9efa27186ee62a5e9
Sebastian Arrubia <sarrubia@gmail.com>
2016-01-10 15:56:19 -0300

    Main SDK class to create client instance

---
+++ src/SplitIO/Sdk.php
@@ -0,0 +1,15 @@
+<?php
+namespace SplitIO;
+
+class Sdk
+{
+    const VERSION = '0.0.1';
+
+    /** @var array Arguments for creating clients */
+    private $args;
+
+    public static function factory(array $args=[])
+    {
+
+    }
+}
