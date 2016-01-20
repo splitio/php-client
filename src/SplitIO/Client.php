fdb1c188a2782b9827d0916f57f423b22e725008
Sebastian Arrubia <sarrubia@gmail.com>
2016-01-19 21:47:07 -0300

    Client to fetch data from server

---
+++ src/SplitIO/Client.php
@@ -0,0 +1,39 @@
+<?php
+namespace SplitIO;
+
+use SplitIO\Http\Client as HttpClient;
+use SplitIO\Http\MethodEnum;
+use SplitIO\Http\Request;
+
+class Client
+{
+    private $authorization = null;
+
+    public function __construct($auth)
+    {
+        $this->authorization = $auth;
+    }
+
+    /**
+     * @return bool|null
+     */
+    public function getSplitChanges()
+    {
+        $httpClient = new HttpClient();
+
+        $request = new Request(MethodEnum::GET(), 'http://localhost:8081/api/splitChanges');
+        $request->setHeader('Authorization', $this->authorization);
+        $request->setHeader('SplitSDKVersion', 'php-0.0.1');
+
+        $response = $httpClient->send($request);
+
+        if ($response->isSuccess()) {
+            return $response->getBody();
+        }
+
+        return false;
+    }
+
+
+
+}
