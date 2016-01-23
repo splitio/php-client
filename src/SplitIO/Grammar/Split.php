ba410a57cd8f38bb892229d9311461b9184b358a
Sebastian Arrubia <sarrubia@gmail.com>
2016-01-23 20:28:02 -0300

    Added method to work with the Split data

--- src/SplitIO/Grammar/Split.php
+++ src/SplitIO/Grammar/Split.php
@@ -39,6 +39,8 @@ namespace SplitIO\Grammar;
     }
 */
 
+use SplitIO\Common\Di;
+
 class Split
 {
     private $orgId = null;
@@ -61,7 +63,7 @@ class Split
 
     public function __construct(array $split)
     {
-        \SplitIO\Common\Di::getInstance()->getLogger()->debug(print_r($split, true));
+        Di::getInstance()->getLogger()->debug(print_r($split, true));
 
         $this->orgId = $split['orgId'];
         $this->environment = $split['environment'];
@@ -72,6 +74,8 @@ class Split
         $this->status = $split['status'];
         $this->killed = $split['killed'];
 
+        Di::getInstance()->getLogger()->info("Constructing Split: ".$this->name);
+
         if (isset($split['conditions']) && is_array($split['conditions'])) {
             $this->conditions = array();
             foreach ($split['conditions'] as $condition) {
@@ -80,11 +84,33 @@ class Split
         }
     }
 
+    public function getName()
+    {
+        return $this->name;
+    }
+
     /**
      * @return array|null
      */
     public function getConditions()
     {
         return $this->conditions;
+    }
+
+    public function getSeed()
+    {
+        return $this->seed;
+    }
+
+    public function getInvolvedUsers()
+    {
+        $users = [];
+        foreach ($this->conditions as $condition) {
+            if ($condition instanceof \SplitIO\Grammar\Condition) {
+                $users = array_merge($users, $condition->getInvolvedUsers());
+            }
+        }
+
+        return array_unique($users, SORT_STRING);
     }
 }
