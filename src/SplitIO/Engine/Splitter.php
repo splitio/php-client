b200d37f31c3161e810e25163cfe2e68c007e7ff
Sebastian Arrubia <sarrubia@gmail.com>
2016-01-23 20:24:46 -0300

    Added getTreatment method

--- src/SplitIO/Engine/Splitter.php
+++ src/SplitIO/Engine/Splitter.php
@@ -2,7 +2,32 @@
 namespace SplitIO\Engine;
 
 
+use SplitIO\Common\Di;
+use SplitIO\Grammar\Condition\Partition;
+use SplitIO\Grammar\Condition\Partition\TreatmentEnum;
+
 class Splitter
 {
+    public static function getTreatment($userId, $seed, $partitions)
+    {
+        Di::getInstance()->getLogger()->info("Splitter evaluating partitions");
 
+        Di::getInstance()->getLogger()->info("UserID: ".$userId);
+        Di::getInstance()->getLogger()->info("Seed: ".$seed);
+        Di::getInstance()->getLogger()->info("Partitions: ".print_r($partitions, true));
+
+        $bucket = (\SplitIO\hash($userId, $seed) % 100) + 1;
+
+        Di::getInstance()->getLogger()->info("Butcket: ".$bucket);
+
+        foreach ($partitions as $partition) {
+            if ($partition instanceof Partition) {
+                if ($bucket <= $partition->getSize()) {
+                    return $partition->getTreatment()->getValue();
+                }
+            }
+        }
+
+        return TreatmentEnum::OFF;
+    }
 }
