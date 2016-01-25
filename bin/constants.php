531512a6947a87d3ecc8f813626184011d6a49a4
Sebastian Arrubia <sarrubia@gmail.com>
2016-01-25 00:27:18 -0300

    PHP service, added support for params and configuration ini file

---
+++ bin/constants.php
@@ -0,0 +1,44 @@
+<?php
+
+//Split.io
+define("SPLITIO_URL","http://localhost:8081");
+
+//Di keys
+define("SPLIT_DI_CONFIG_KEY", "SplitIO_cli_Config");
+
+//Config array keys
+define("SPLITIO_CONFIG_API_KEY", "cnf-api-key");
+define("SPLITIO_CONFIG_FILE", "cnf-config-file");
+define("SPLITIO_CONFIG_LOG_ADAPTER", "cnf-log-adapter");
+define("SPLITIO_CONFIG_LOG_LEVEL", "cnf-log-level");
+define("SPLITIO_CONFIG_CACHE_TTL", "cnf-cache-ttl");
+define("SPLITIO_CONFIG_CACHE_ADAPTER", "cnf-cache-adapter");
+define("SPLITIO_CONFIG_CACHE_FILESYSTEM_PATH", "cnf-cache-filesystem-path");
+define("SPLITIO_CONFIG_CACHE_REDIS_HOST", "cnf-cache-redis-host");
+define("SPLITIO_CONFIG_CACHE_REDIS_PORT", "cnf-cache-redis-port");
+
+
+//Output defined colors
+//Foreground colors
+define("SPLIT_FG_BLACK", '0;30');
+define("SPLIT_FG_DARK_GRAY", '1;30');
+define("SPLIT_FG_BLUE", '0;34');
+define("SPLIT_FG_LIGHT_BLUE", '1;34');
+define("SPLIT_FG_GREEN", '0;32');
+define("SPLIT_FG_LIGHT_GREEN", '1;32');
+define("SPLIT_FG_CYAN", '0;36');
+define("SPLIT_FG_RED", '0;31');
+define("SPLIT_FG_PURPLE", '0;35');
+define("SPLIT_FG_BROWN", '0;33');
+define("SPLIT_FG_YELLOW", '1;33');
+define("SPLIT_FG_LIGHT_GRAY", '0;37');
+define("SPLIT_FG_WHITE", '1;37');
+//Background colors
+define("SPLIT_BG_BLACK", '40');
+define("SPLIT_BG_RED", '41');
+define("SPLIT_BG_GREEN", '42');
+define("SPLIT_BG_YELLOW", '43');
+define("SPLIT_BG_BLUE", '44');
+define("SPLIT_BG_MAGENTA", '45');
+define("SPLIT_BG_CYAN", '46');
+define("SPLIT_BG_LIGHT_GRAY", '47');
