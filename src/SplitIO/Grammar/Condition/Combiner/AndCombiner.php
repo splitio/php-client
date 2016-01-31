82f9d8f16c97568c57ce2528af23e5efa10caf1c
Sebastian Arrubia <sarrubia@gmail.com>
2016-01-31 15:42:31 -0300

    modified logic on AND combiner

--- src/SplitIO/Grammar/Condition/Combiner/AndCombiner.php
+++ src/SplitIO/Grammar/Condition/Combiner/AndCombiner.php
@@ -2,7 +2,23 @@
 namespace SplitIO\Grammar\Condition\Combiner;
 
 
-class AndCombiner
+class AndCombiner implements CombinerInterface
 {
 
+    public static function evaluate(array $factors)
+    {
+        $return = true;
+        if (!empty($factors)) {
+
+            foreach ($factors as $factor) {
+                if (is_bool($factor)) {
+                    $return = $return && $factor;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return $return;
+    }
 }
