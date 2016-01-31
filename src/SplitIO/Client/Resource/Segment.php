2171bf69b2d0c698dbbf300d233dfbfe6d0519df
Sebastian Arrubia <sarrubia@gmail.com>
2016-01-31 15:39:13 -0300

    Modified approach on Segment Module

--- src/SplitIO/Client/Resource/Segment.php
+++ src/SplitIO/Client/Resource/Segment.php
@@ -3,29 +3,28 @@ namespace SplitIO\Client\Resource;
 
 use SplitIO\Client\ClientBase;
 use SplitIO\Common\Di;
+use SplitIO\Grammar\Condition\Matcher\SegmentData;
 use SplitIO\Http\Client as HttpClient;
 use SplitIO\Http\MethodEnum;
 
 class Segment extends ClientBase
 {
-    const KEY_SINCE_CACHED_ITEM = 'SPLITIO.segment.{segment_name}.since';
+    const KEY_TILL_CACHED_ITEM = 'SPLITIO.segment.{segment_name}.till';
 
-    private $since = -1;
-
-    private $still = null;
+    private $till = -1;
 
     private $servicePath = '/api/segmentChanges/';
 
     public function getSegmentChanges($segmentName)
     {
-        //Fetching since value from cache.
-        $cacheKey = str_replace('{segment_name}', $segmentName, self::KEY_SINCE_CACHED_ITEM);
+        //Fetching next since (till) value from cache.
+        $cacheKey = str_replace('{segment_name}', $segmentName, self::KEY_TILL_CACHED_ITEM);
         $since_cached_item = Di::getInstance()->getCache()->getItem($cacheKey);
 
-        $servicePath = $this->servicePath.$segmentName;
+        $servicePath = $this->servicePath . $segmentName;
 
         if ($since_cached_item->isHit()) {
-            $servicePath .= '?since='.$since_cached_item->get();
+            $servicePath .= '?since=' . $since_cached_item->get();
         }
 
         Di::getInstance()->getLogger()->info("SERVICE PATH: $servicePath");
@@ -38,20 +37,62 @@ class Segment extends ClientBase
         if ($response->isSuccess()) {
             $segment = json_decode($response->getBody(), true);
 
-            $this->since = (isset($segment['since'])) ? $segment['since'] : -1;
+            //Returning false due the server has not changes
+            if (isset($segment['since']) && isset($segment['till']) && $segment['since'] == $segment['till']) {
+                Di::getInstance()->getLogger()->notice("Segments returned by the server are empty");
+                return false;
+            }
 
-            //Updating since value.
-            $since_cached_item->set($this->since);
-            //@TODO set expiration time from config.
-            $since_cached_item->expiresAfter(300);
+            $this->till = (isset($segment['till'])) ? $segment['till'] : -1;
+
+            //Updating next since (till) value.
+            if ($this->till != $since_cached_item->get()) {
+                $since_cached_item->set($this->till);
+            }
+            $since_cached_item->expiresAfter(Di::getInstance()->getSplitSdkConfiguration()->getCacheItemTtl());
+
             Di::getInstance()->getCache()->save($since_cached_item);
-
-            $this->till = (isset($segment['till'])) ? $segment['till'] : null;
 
             return $segment;
         }
 
         return false;
+    }
+
+    public function addSegmentOnCache(SegmentData $segmentData)
+    {
+        $di = Di::getInstance();
+        $cache = $di->getCache();
+
+        $segmentName = $segmentData->getName();
+
+        $segmentDataCacheItem = $cache->getItem(\SplitIO\getCacheKeyForSegmentData($segmentName));
+
+        if ($segmentDataCacheItem->isHit()) { //Update Segment Data.
+
+            $segment = unserialize($segmentDataCacheItem->get());
+
+            if ($segment instanceof SegmentData) {
+
+                $currentUsers = $segment->getAddedUsers();
+                $removedUsers = $segmentData->getRemovedUsers();
+
+                $allUsers = array_merge($currentUsers, $segmentData->getAddedUsers());
+
+                $segment->setAddedUsers(array_diff($allUsers, $removedUsers));
+                $segment->setRemovedUsers($removedUsers);
+                $segment->setSince($segmentData->getSince());
+                $segment->setTill($segmentData->getTill());
+            }
+
+        } else { //Create Segment Data.
+            $segment = $segmentData;
+        }
+
+        $segmentDataCacheItem->set(serialize($segment));
+        $segmentDataCacheItem->expiresAfter(Di::getInstance()->getSplitSdkConfiguration()->getCacheItemTtl());
+
+        return $cache->save($segmentDataCacheItem);
     }
 
 }
