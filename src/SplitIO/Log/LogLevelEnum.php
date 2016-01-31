14750e5b0a68c445f724170e6a92d3443c623d3c
Sebastian Arrubia <sarrubia@gmail.com>
2016-01-31 15:50:57 -0300

    Added log level enumerator

---
+++ src/SplitIO/Log/LogLevelEnum.php
@@ -0,0 +1,16 @@
+<?php
+namespace SplitIO\Log;
+
+use SplitIO\Common\Enum;
+
+class LogLevelEnum extends Enum
+{
+    const EMERGENCY = 'emergency';
+    const ALERT = 'alert';
+    const CRITICAL = 'critical';
+    const ERROR = 'error';
+    const WARNING = 'warning';
+    const NOTICE = 'notice';
+    const INFO = 'info';
+    const DEBUG = 'debug';
+}
