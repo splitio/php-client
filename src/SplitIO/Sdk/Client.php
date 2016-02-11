15dad3fca1c1168dde06e5605a5cf5ba25fbec71
Sebastian Arrubia <sarrubia@gmail.com>
2016-02-11 11:32:06 -0800

    Added isTreatment and getTreatment to SDK

--- src/SplitIO/Sdk/Client.php
+++ src/SplitIO/Sdk/Client.php
@@ -4,6 +4,7 @@ namespace SplitIO\Sdk;
 use SplitIO\Cache\SplitCache;
 use SplitIO\Common\Di;
 use SplitIO\Engine;
+use SplitIO\Grammar\Condition\Partition\TreatmentEnum;
 use SplitIO\Grammar\Split;
 
 class Client
@@ -25,5 +26,52 @@ class Client
 
         Di::getInstance()->getLogger()->info("Returning FALSE - $featureName is not on cache");
         return false;
+    }
+
+    private function getTratmentFromSplit($key, $featureName)
+    {
+        $splitCacheKey = SplitCache::getCacheKeyForSplit($featureName);
+        $splitCachedItem = Di::getInstance()->getCache()->getItem($splitCacheKey);
+
+        if ($splitCachedItem->isHit()) {
+            Di::getInstance()->getLogger()->info("$featureName is present on cache");
+            $splitRepresentation = $splitCachedItem->get();
+
+            $split = new Split(json_decode($splitRepresentation, true));
+
+            return Engine::getTreatment($key, $split);
+        }
+
+        return false;
+    }
+
+    public function isTreatment($key, $featureName, $treatment)
+    {
+        $calculatedTreatment = $this->getTratmentFromSplit($key, $featureName);
+
+        if ($calculatedTreatment !== false) {
+            if ($treatment == $calculatedTreatment) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        return false;
+    }
+
+    public function getTreatment($key, $featureName, $defaultTreatment)
+    {
+        $calculatedTreatment = $this->getTratmentFromSplit($key, $featureName);
+
+        if ($calculatedTreatment !== false) {
+            if ($calculatedTreatment != TreatmentEnum::CONTROL) {
+                return $calculatedTreatment;
+            } else {
+                return $defaultTreatment;
+            }
+        }
+
+        return $defaultTreatment;
     }
 }
