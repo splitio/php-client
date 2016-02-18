0ce792c212837e2def028696bc615962b0584ff0
Sebastian Arrubia <sarrubia@gmail.com>
2016-02-17 21:08:32 -0300

    Added comments on Client methods and removed isOn

--- src/SplitIO/Sdk/Client.php
+++ src/SplitIO/Sdk/Client.php
@@ -9,40 +9,38 @@ use SplitIO\Grammar\Split;
 
 class Client
 {
-
-    public function isOn($key, $featureName)
-    {
-        $splitCacheKey = SplitCache::getCacheKeyForSplit($featureName);
-        $splitCachedItem = Di::getInstance()->getCache()->getItem($splitCacheKey);
-
-        if ($splitCachedItem->isHit()) {
-            Di::getInstance()->getLogger()->info("$featureName is present on cache");
-            $splitRepresentation = $splitCachedItem->get();
-
-            $split = new Split(json_decode($splitRepresentation, true));
-
-            return Engine::isOn($key, $split);
-        }
-
-        Di::getInstance()->getLogger()->info("Returning FALSE - $featureName is not on cache");
-        return false;
-    }
-
-    public function isTreatment($key, $featureName, $treatment)
-    {
-        $calculatedTreatment = $this->getTreatment($key, $featureName);
-
-        if ($calculatedTreatment !== null) {
-            if ($treatment == $calculatedTreatment) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-        return false;
-    }
-
+    /**
+     * Returns the treatment to show this id for this feature.
+     * The set of treatments for a feature can be configured
+     * on the Split web console.
+     * This method returns the string 'control' if:
+     * <ol>
+     *     <li>Any of the parameters were null</li>
+     *     <li>There was an exception</li>
+     *     <li>The SDK does not know this feature</li>
+     *     <li>The feature was deleted through the web console.</li>
+     * </ol>
+     * 'control' is a reserved treatment, to highlight these
+     * exceptional circumstances.
+     *
+     * <p>
+     * The sdk returns the default treatment of this feature if:
+     * <ol>
+     *     <li>The feature was killed</li>
+     *     <li>The id did not match any of the conditions in the
+     * feature roll-out plan</li>
+     * </ol>
+     * The default treatment of a feature is set on the Split web
+     * console.
+     *
+     * <p>
+     * This method does not throw any exceptions.
+     * It also never  returns null.
+     *
+     * @param $key
+     * @param $featureName
+     * @return string
+     */
     public function getTreatment($key, $featureName)
     {
         $splitCacheKey = SplitCache::getCacheKeyForSplit($featureName);
@@ -65,6 +63,43 @@ class Client
             return $split->getDefaultTratment();
         }
 
-        return null;
+        return TreatmentEnum::CONTROL;
+    }
+
+    /**
+     * A short-hand for
+     * <pre>
+     *     (getTreatment(key, feature) == treatment) ? true : false;
+     * </pre>
+     *
+     * This method never throws exceptions.
+     * Instead of throwing  exceptions, it returns false.
+     *
+     * @param $key
+     * @param $featureName
+     * @param $treatment
+     * @return bool
+     */
+    public function isTreatment($key, $featureName, $treatment)
+    {
+        try {
+
+            $calculatedTreatment = $this->getTreatment($key, $featureName);
+
+            if ($calculatedTreatment !== TreatmentEnum::CONTROL) {
+                if ($treatment == $calculatedTreatment) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+        } catch (\Exception $e) {
+            Di::getInstance()->getLogger()->critical("SDK Client on isTreatment is critical");
+            Di::getInstance()->getLogger()->critical($e->getMessage());
+            Di::getInstance()->getLogger()->critical($e->getTraceAsString());
+        }
+
+        return false;
     }
 }
