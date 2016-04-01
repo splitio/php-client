9e8138f58d2cac04712a793df826210f12591824
Sebastian Arrubia <sarrubia@gmail.com>
2016-04-01 18:06:50 -0300

    Adding Data type enumeration for matchers

---
+++ src/SplitIO/Grammar/Condition/Matcher/DataTypeEnum.php
@@ -0,0 +1,11 @@
+<?php
+namespace SplitIO\Grammar\Condition\Matcher;
+
+use SplitIO\Component\Common\Enum;
+
+class DataTypeEnum extends Enum
+{
+    const DATETIME = 'DATETIME';
+
+    const NUMBER = 'NUMBER';
+}
