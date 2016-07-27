737597a73b3a862fca4760eac2b9b4f10d2baee2
Sebastian Arrubia <sarrubia@gmail.com>
2016-07-27 20:58:49 -0300

    Fixing attributes test for PHP 5.3

--- tests/Suite/Attributes/SdkAttributesTest.php
+++ tests/Suite/Attributes/SdkAttributesTest.php
@@ -48,9 +48,13 @@ class SdkAttributesTest extends \PHPUnit_Framework_TestCase
         //Testing version string
         $this->assertTrue(is_string(\SplitIO\version()));
 
+
+        $parameters = array('scheme' => 'redis', 'host' => REDIS_HOST, 'port' => REDIS_PORT, 'timeout' => 881);
+        $options = array();
+
         $sdkConfig = [
             'log' => ['adapter' => 'stdout', 'level' => 'info'],
-            'cache' => ['adapter' => 'redis', 'options' => ['host' => REDIS_HOST, 'port' => REDIS_PORT]]
+            'cache' => array('adapter' => 'predis', 'parameters' => $parameters, 'options' => $options)
         ];
 
         //Initializing the SDK instance.
