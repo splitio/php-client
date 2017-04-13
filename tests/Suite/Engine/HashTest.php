d7424b3d8070f443f648e63109a3f73a0dbca423
Martin Redolatti <martinredolatti@gmail.com>
2017-04-13 15:28:41 -0300

    Test algo field

--- tests/Suite/Engine/HashTest.php
+++ tests/Suite/Engine/HashTest.php
@@ -1,8 +1,13 @@
 <?php
 namespace SplitIO\Test\Suite\Engine;
 
+use SplitIO\Engine\Hash\HashFactory;
 use SplitIO\Engine\Hash\LegacyHash;
 use SplitIO\Engine\Hash\Murmur3Hash;
+use SplitIO\Component\Cache\SplitCache;
+use SplitIO\Engine\Hash\HashAlgorithmEnum;
+use SplitIO\Grammar\Split;
+use SplitIO\Split as SplitApp;
 
 class HashTest extends \PHPUnit_Framework_TestCase
 {
@@ -66,6 +71,72 @@ class HashTest extends \PHPUnit_Framework_TestCase
             } else {
                 $this->assertTrue(false, "Sample Data not found");
             }
+        }
+    }
+
+    private function addSplitsInCache()
+    {
+        $splitChanges = file_get_contents(__DIR__."/../../files/algoSplits.json");
+        $this->assertJson($splitChanges);
+
+        $splitCache = new SplitCache();
+
+        $splitChanges = json_decode($splitChanges, true);
+        $splits = $splitChanges['splits'];
+
+        foreach ($splits as $split) {
+            $splitName = $split['name'];
+            $this->assertTrue($splitCache->addSplit($splitName, json_encode($split)));
+        }
+    }
+
+    public function testAlgoField()
+    {
+        $parameters = array('scheme' => 'redis', 'host' => REDIS_HOST, 'port' => REDIS_PORT, 'timeout' => 881);
+        $options = array();
+
+        $sdkConfig = array(
+            'log' => array('adapter' => 'stdout'),
+            'cache' => array('adapter' => 'predis', 'parameters' => $parameters, 'options' => $options)
+        );
+
+        //Initializing the SDK instance.
+        $splitFactory = \SplitIO\Sdk::factory('asdqwe123456', $sdkConfig);
+        $splitFactory->client();
+
+        //Populating the cache.
+        $this->addSplitsInCache();
+
+        $cases = array(
+            array(  // Split with algo = 1. Should use legacy function
+                'key' => 'some_feature_1',
+                'algo' => HashAlgorithmEnum::LEGACY,
+                'class' => '\SplitIO\Engine\Hash\LegacyHash'
+            ),
+            array(  // Split with algo = 2. Should use murmur function
+                'key' => 'some_feature_2',
+                'algo' => HashAlgorithmEnum::MURMUR,
+                'class' => '\SplitIO\Engine\Hash\Murmur3Hash'
+            ),
+            array(  // Split with algo = null. Should use legacy function
+                'key' => 'some_feature_3',
+                'algo' => HashAlgorithmEnum::LEGACY,
+                'class' => '\SplitIO\Engine\Hash\LegacyHash'
+            ),
+            array(  // Split with no algo field. SHould use legacy function
+                'key' => 'some_feature_4',
+                'algo' => HashAlgorithmEnum::LEGACY,
+                'class' => '\SplitIO\Engine\Hash\LegacyHash'
+            ),
+        );
+
+        foreach ($cases as $case) {
+            $splitCacheKey = SplitCache::getCacheKeyForSplit($case['key']);
+            $splitCachedItem = SplitApp::cache()->getItem($splitCacheKey);
+            $split = new Split(json_decode($splitCachedItem->get(), true));
+            $this->assertEquals($split->getAlgo(), $case['algo']);
+            $hasher = HashFactory::getHashAlgorithm($split->getAlgo());
+            $this->assertInstanceof($case['class'], $hasher);
         }
     }
 }
