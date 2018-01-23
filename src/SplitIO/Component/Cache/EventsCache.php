ba11b982e2f4275613c03cc9186c081f19b6ff5d
Sebastian Arrubia <sarrubia@gmail.com>
2018-01-23 15:47:58 -0300

    Adding cache objects

---
+++ src/SplitIO/Component/Cache/EventsCache.php
@@ -0,0 +1,15 @@
+<?php
+namespace SplitIO\Component\Cache;
+
+use SplitIO\Component\Common\Di;
+use SplitIO\Sdk\Events\EventQueueMessage;
+
+class EventsCache
+{
+    const KEY_EVENTS_LIST = "SPLITIO.events";
+
+    public static function addEvent(EventQueueMessage $message)
+    {
+        return Di::getCache()->rightPushInList(self::KEY_EVENTS_LIST, json_encode($message->toArray()));
+    }
+}
