17eb8b2e5354bd028ef5aaa5a2a1cbc428b46940
Matias Melograno <matiasmelograno@Sebastians-MBP.localdomain>
2018-07-26 16:42:12 -0300

    added Impression Listener feature to send data to Client

--- src/SplitIO/Sdk/Client.php
+++ src/SplitIO/Sdk/Client.php
@@ -19,6 +19,7 @@ class Client implements ClientInterface
 {
 
     private $evaluator = null;
+    private $impressionListener = null;
 
     /**
      * Flag to get Impression's labels feature enabled
@@ -33,10 +34,15 @@ class Client implements ClientInterface
     {
         $this->labelsEnabled = isset($options['labelsEnabled']) ? $options['labelsEnabled'] : true;
         $this->evaluator = new Evaluator($options);
+
+        if (isset($options['impressionListener'])) {
+            $this->impressionListener = new \SplitIO\Sdk\ImpressionListenerWrapper($options['impressionListener']);
+        }
     }
 
-
     /**
+     * Builds new Impression object
+     * 
      * @param $matchingKey
      * @param $feature
      * @param $treatment
@@ -44,8 +50,10 @@ class Client implements ClientInterface
      * @param null $time
      * @param int $changeNumber
      * @param string $bucketingKey
+     * 
+     * @return \SplitIO\Sdk\Impressions\Impression
      */
-    private function logImpression(
+    private function createImpression(
         $matchingKey,
         $feature,
         $treatment,
@@ -54,7 +62,6 @@ class Client implements ClientInterface
         $changeNumber = -1,
         $time = null
     ) {
-
         if (!$this->labelsEnabled) {
             $label = null;
         }
@@ -68,6 +75,15 @@ class Client implements ClientInterface
             $changeNumber,
             $bucketingKey
         );
+
+        return $impression;
+    }
+
+    /**
+     * @param $impression \SplitIO\Sdk\Impressions\Impression
+     */
+    private function logImpression(Impression $impression) 
+    {
         TreatmentImpression::log($impression);
     }
 
@@ -126,8 +142,8 @@ class Client implements ClientInterface
         try {
             $result = $this->evaluator->evalTreatment($matchingKey, $bucketingKey, $featureName, $attributes);
 
-            // Register impression
-            $this->logImpression(
+            // Creates impression
+            $impression = $this->createImpression(
                 $matchingKey,
                 $featureName,
                 $result['treatment'],
@@ -135,6 +151,13 @@ class Client implements ClientInterface
                 $bucketingKey,
                 $result['impression']['changeNumber']
             );
+
+            // Provides logic to send data to Client
+            $this->impressionListener->sendDataToClient($impression, $attributes);
+
+            // Register impression
+            $this->logImpression($impression);
+
             //Register latency value
             MetricsCache::addLatencyOnBucket(
                 Metrics::MNAME_SDK_GET_TREATMENT,
@@ -152,13 +175,20 @@ class Client implements ClientInterface
         }
 
         try {
-            $this->logImpression(
+            // Creates impression
+            $impression = $this->createImpression(
                 $matchingKey,
                 $featureName,
                 TreatmentEnum::CONTROL,
                 $impressionLabel,
                 $bucketingKey
             );
+
+            // Provides logic to send data to Client
+            $this->impressionListener->sendDataToClient($impression, $attributes);
+
+            // Register impression
+            $this->logImpression($impression);
         } catch (\Exception $e) {
             SplitApp::logger()->critical(
                 "An error occurred when attempting to log impression for " .
---
+++ src/SplitIO/Sdk/ImpressionListenerWrapper.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace SplitIO\Sdk;
+use SplitIO\Sdk\Impressions\Impression;
+use SplitIO\Split as SplitApp;
+
+// Declaration of ImpressionListenerWrapper class
+class ImpressionListenerWrapper
+{
+    private $impressionListener = null;
+
+    /**
+     * Constructor
+     *
+     * @param   $impressionListener   ImpressionListener  client's instance of that implements ImpressionListener
+     *
+     * @return \SplitIO\Sdk\ImpressionListenerWrapper
+     */
+    public function __construct(ImpressionListener $impressionListener)
+    {
+        // Checks if the class method exists
+        if (method_exists($impressionListener, 'readImpression')) {
+            SplitApp::logger()->info('Method readImpression exists in custom '
+                .'user defined class for ImpressionListener.');
+            // Sets client's class to impressionListener
+            $this->impressionListener = $impressionListener;
+        } else {
+            SplitApp::logger()->error('Method readImpression does not exist in custom '
+                .'user defined class for ImpressionListener.');
+        }
+    }
+
+    /*
+     * Builds the result object to be passed to client for doing custom logic with it.
+     *
+     * @param   $impression   \SplitIO\Sdk\Impressions\Impression   Impression instance as result of treatment:
+     *  $impression['$id']                  =>  string   Identity's id
+     *  $impression['feature']              =>  string   Identity's feature
+     *  $impression['treatment']            =>  string   Identity's treatment result
+     *  $impression['label']                =>  string   Identity's label
+     *  $impression['time']                 =>  integer  Identity's organization Id
+     *  $impression['changeNumber']         =>  number   Identity's change number
+     *  $impression['bucketingKey']         =>  string   Identity's bucketing key
+     *
+     * @param   $attributes   mixed     It will corresponds to every data that user wants to send.
+     *
+     * NOTE: A try/catch has been placed here to avoid any issue that could break the Sdk logic.
+     */
+    public function sendDataToClient(Impression $impression, $attributes) {
+        // Builds data to send to client
+        $data = array(
+            'impression' => $impression,
+            'attributes' => $attributes,
+            'instance-id' => \SplitIO\getHostIpAddress(),
+            'sdk-language-version' => 'php-' . \SplitIO\version()
+        );
+
+        try {
+            // Executes client's custom method
+            $this->impressionListener->readImpression($data);
+        } catch (\Throwable $e) {
+            SplitApp::logger()->error('readImpression user\'s method is throwing exceptions');
+            SplitApp::logger()->error($e->getMessage());
+            SplitApp::logger()->error($e->getTraceAsString());
+        }
+    }
+}
