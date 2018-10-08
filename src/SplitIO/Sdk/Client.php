e241e207e2a5df2f0a5421e169183bbd2063429a
Matias Melograno <matiasmelograno@split.io>
2018-10-08 13:04:52 -0300

    added get treatments method

--- src/SplitIO/Sdk/Client.php
+++ src/SplitIO/Sdk/Client.php
@@ -191,6 +191,71 @@ class Client implements ClientInterface
     }
 
     /**
+     * Returns the treatment to show this id for this feature.
+     * The set of treatments for a feature can be configured
+     * on the Split web console.
+     * This method returns the string 'control' if:
+     * <ol>
+     *     <li>Any of the parameters were null</li>
+     *     <li>There was an exception</li>
+     *     <li>The SDK does not know this feature</li>
+     *     <li>The feature was deleted through the web console.</li>
+     * </ol>
+     * 'control' is a reserved treatment, to highlight these
+     * exceptional circumstances.
+     *
+     * <p>
+     * The sdk returns the default treatment of this feature if:
+     * <ol>
+     *     <li>The feature was killed</li>
+     *     <li>The id did not match any of the conditions in the
+     * feature roll-out plan</li>
+     * </ol>
+     * The default treatment of a feature is set on the Split web
+     * console.
+     *
+     * <p>
+     * This method does not throw any exceptions.
+     * It also never  returns null.
+     *
+     * @param $key
+     * @param $featureName
+     * @param $attributes
+     * @return string
+     */
+    public function getTreatments($key, array $featureNames, array $attributes = null)
+    {
+        #$key = InputValidator::validateKey($key);
+        #$featureName = InputValidator::validateFeatureName($featureName);
+
+        #if (is_null($key) || is_null($featureName)) {
+            #return TreatmentEnum::CONTROL;
+        #}
+
+        #$matchingKey = $key['matchingKey'];
+        #$bucketingKey = $key['bucketingKey'];
+
+        try {
+            $featureNames = array_values(array_filter($featureNames));
+            echo $featureNames . "\n";
+            $result = array();
+
+            for ($i = 0; $i < count($featureNames); $i++) {
+                $featureName = $featureNames[$i];
+                $result[$featureName] = $this->getTreatment($key, $featureName, $attributes);
+            }
+
+            return $result;
+        } catch (\Exception $e) {
+            SplitApp::logger()->critical('getTreatments method is throwing exceptions');
+            SplitApp::logger()->critical($e->getMessage());
+            SplitApp::logger()->critical($e->getTraceAsString());
+        }
+
+        return TreatmentEnum::CONTROL;
+    }
+
+    /**
      * A short-hand for
      * <pre>
      *     (getTreatment(key, feature) == treatment) ? true : false;
