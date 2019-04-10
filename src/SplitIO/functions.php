91918895f9e1dd2720a2b47d40a3dd68689f0ac5
Matias Melograno <matiasmelograno@split.io>
2019-04-10 19:07:59 -0300

    added support for localhost mode with yaml files

--- src/SplitIO/functions.php
+++ src/SplitIO/functions.php
@@ -60,7 +60,7 @@ function parseSplitsFile($fileContent)
             $matches = array();
             if (preg_match($re, $line, $matches)) {
                 if (isset($matches[1]) && isset($matches[2])) {
-                    $result[$matches[1]] = $matches[2];
+                    $result[$matches[1]]["treatment"] = $matches[2];
                 }
             }
         }
