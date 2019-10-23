a7009031fa375104fdec24f55d8cab2ea48f8a75
Cristian Daniel Fadón <cristian.fadon@split.io>
2019-10-23 19:02:01 -0300

    Cosmetics

--- sonar-scanner.sh
+++ sonar-scanner.sh
@@ -1,11 +1,5 @@
 #/bin/bash -e
 
-if [ "$TRAVIS_BRANCH" == 'develop' ]; then
-  TARGET_BRANCH='master'
-else
-  TARGET_BRANCH='develop'
-fi
-
 sonar_scanner() {
   local params="$@"
 
@@ -18,15 +12,25 @@ sonar_scanner() {
     -Dsonar.exclusions='**/tests/**/*.*' \
     -Dsonar.links.ci='https://travis-ci.com/splitio/php-client' \
     -Dsonar.links.scm='https://github.com/splitio/php-client' \
-    -Dsonar.pullrequest.provider='GitHub' \
-    -Dsonar.pullrequest.github.repository='splitio/php-client' \
     "${params}"
 
   return $?
 }
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  sonar_scanner -Dsonar.pullrequest.key=$TRAVIS_PULL_REQUEST -Dsonar.pullrequest.branch=$TRAVIS_PULL_REQUEST_BRANCH -Dsonar.pullrequest.base=$TRAVIS_BRANCH
+  sonar_scanner \
+    -Dsonar.pullrequest.provider='GitHub' \
+    -Dsonar.pullrequest.github.repository='splitio/php-client' \
+    -Dsonar.pullrequest.key=$TRAVIS_PULL_REQUEST \
+    -Dsonar.pullrequest.branch=$TRAVIS_PULL_REQUEST_BRANCH \
+    -Dsonar.pullrequest.base=$TRAVIS_BRANCH
 else
-  sonar_scanner -Dsonar.branch.name=$TRAVIS_BRANCH -Dsonar.branch.target=$TARGET_BRANCH
+  if [ "$TRAVIS_BRANCH" == 'develop' ]; then
+    TARGET_BRANCH='master'
+  else
+    TARGET_BRANCH='develop'
+  fi
+  sonar_scanner \
+    -Dsonar.branch.name=$TRAVIS_BRANCH \
+    -Dsonar.branch.target=$TARGET_BRANCH
 fi
