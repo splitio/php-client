4f0c6a0a3764f8b9deeced07baed68b245f2cfaa
Cristian Daniel Fadón <cristian.fadon@split.io>
2019-10-23 18:09:39 -0300

    Send report to SonarQube

---
+++ sonar-scanner.sh
@@ -0,0 +1,29 @@
+#/bin/bash -e
+
+if [ "$TRAVIS_BRANCH" == 'develop' ]; then
+  TARGET_BRANCH='master'
+else
+  TARGET_BRANCH='develop'
+fi
+
+sonar_scanner() {
+  local params="$@"
+
+  vendor/bin/sonar-scanner \
+    -Dsonar.host.url='https://sonarqube.split-internal.com' \
+    -Dsonar.login=$SONAR_TOKEN \
+    -Dsonar.ws.timeout='300' \
+    -Dsonar.projectName='php-client' \
+    -Dsonar.exclusions="**/tests/**/*.*" \
+    -Dsonar.links.ci='https://travis-ci.com/splitio/php-client' \
+    -Dsonar.links.scm='https://github.com/splitio/php-client' \
+    "${params}"
+
+  return $?
+}
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  sonar_scanner -Dsonar.pullrequest.key=$TRAVIS_PULL_REQUEST -Dsonar.pullrequest.branch=$TRAVIS_PULL_REQUEST_BRANCH -Dsonar.pullrequest.base=$TRAVIS_BRANCH
+else
+  sonar_scanner -Dsonar.branch.name=$TRAVIS_BRANCH -Dsonar.branch.target=$TARGET_BRANCH
+fi
