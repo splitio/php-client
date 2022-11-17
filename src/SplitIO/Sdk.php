5c7e7540af283a020b11a139e3a589e832b38a7a
Matias Melograno <matias.melograno@split.io>
2022-11-17 19:27:06 -0300

    removed comments

--- src/SplitIO/Sdk.php
+++ src/SplitIO/Sdk.php
@@ -83,24 +83,6 @@ class Sdk
             throw new Exception("A valid cache system is required. Given: $cacheAdapter");
         }
 
-        /*
-        $adapter_config = array(
-            'name' => 'predis',
-            'options' => array(
-                'options'               => isset($options['predis-options'])
-                    ? $options['predis-options'] : null,
-                'parameters'            => isset($options['predis-parameters'])
-                    ? $options['predis-parameters'] : null,
-                'sentinels'             => isset($options['predis-sentinels'])
-                    ? $options['predis-sentinels'] : null,
-                'clusterNodes'          => isset($options['predis-clusterNodes'])
-                    ? $options['predis-clusterNodes'] : null,
-                'distributedStrategy'   => isset($options['predis-distributedStrategy'])
-                    ? $options['predis-distributedStrategy'] : null,
-            )
-        );
-        */
-
         $adapter_config = array(
             'name' => 'predis',
             'options' => $_options,
